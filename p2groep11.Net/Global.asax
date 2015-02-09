6aed3de3e5b6fbf571b350f9734b2a8b33f88be3
208134ld <l.dupont@hotmail.be>
2015-02-09 15:25:15 +0100

    #1: Initial Setup

---
+++ p2groep11.Net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="p2groep11.Net.MvcApplication" Language="C#" %>
---
+++ p2groep11.Net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(p2groep11.Net.Startup))]
+namespace p2groep11.Net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
