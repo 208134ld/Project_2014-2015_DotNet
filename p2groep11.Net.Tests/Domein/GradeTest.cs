9e89239361eea3f6c913947090dd915d9bfc76fb
yoloswagings <arnedebremme@hotmail.com>
2015-02-10 16:54:47 +0100

    GradeTest aanpassen aan nieuw ontwerp

--- p2groep11.Net.Tests/Domein/GradeTest.cs
+++ p2groep11.Net.Tests/Domein/GradeTest.cs
@@ -15,50 +15,42 @@ namespace p2groep11.Net.Tests.Domein
         }
         [TestMethod]
        public void setGraadSetsGraadl()        {
-            grade.Graad = 1;
-            Assert.AreEqual(1,grade.Graad);
+            grade.Number = 1;
+            Assert.AreEqual(1,grade.Number);
         }
         [TestMethod]
         [ExpectedException(typeof(ArgumentException), "Graad moet tussen 1 en 3 liggen")]
         public void graadKanNiet0zijn()
         {
-            grade.Graad = 0;
+            grade.Number = 0;
         }
 
         [TestMethod]
         [ExpectedException(typeof (ArgumentException), "Graad moet tussen 1 en 3 liggen")]
         public void graadKanNiet4Zijn()
         {
-            grade.Graad = 4;
+            grade.Number = 4;
         }
-
-        [TestMethod]
-        [ExpectedException(typeof (ArgumentException), "Graad mag niet null zijn")]
-        public void graadKanNietNullZijn()
-        {
-            grade.Graad = null;
-        }
-
         [TestMethod]
         [ExpectedException(typeof (ArgumentException), "Graad moet tussen 1 en 3 liggen")]
         public void graadKanNietGroterZijnDan3()        {
-            grade.Graad = 1200;
+            grade.Number= 1200;
         }
 
         [TestMethod]
         public void setFormSetsFormIfGradeIs2()
         {
-            grade.Graad = 2;
-            grade.Form = Form.EersteLeerjaar;
-            Assert.AreEqual(Form.EersteLeerjaar,grade.Form);
+            grade.Number = 2;
+            grade.From = Form.eerste;
+            Assert.AreEqual(Form.eerste,grade.From);
         }
 
         [TestMethod]
         [ExpectedException(typeof (ArgumentException), "Grade.form throws exception als graad niet=2")]
         public void setFormThrowsExceptionWhenNot2()
         {
-            grade.Graad = 1;
-            grade.Form = Form.EersteLeerjaar;
+            grade.Number = 1;
+            grade.From = Form.eerste;
         }
    }
 }
