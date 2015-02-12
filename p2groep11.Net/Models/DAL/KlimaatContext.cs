c2934871f3508145bc45ba0243316ffa7226f83b
stijn26 <stijnaerts32@gmail.com>
2015-02-12 09:50:39 +0100

    #6 Alles verwijdert van login-systeem

--- p2groep11.Net/Models/DAL/KlimaatContext.cs
+++ p2groep11.Net/Models/DAL/KlimaatContext.cs
@@ -1,16 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
+using System.Reflection;
 using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace p2groep11.Net.Models.DAL
 {
-    public class KlimaatContext : ApplicationDbContext
+    public class KlimaatContext : IdentityDbContext<ApplicationUser>
     {
-
-        public KlimaatContext()
+        public StuHoGentContext()
+            : base("StuHoGent")
         {
-            
+
+        }
+        public DbSet<Werknemer> Werknemers { get; set; }
+        public DbSet<Reden> Redenen { get; set; }
+        public DbSet<Afwezigheid> Afwezigheden { get; set; }
+
+        public static StuHoGentContext Create()
+        {
+            return new StuHoGentContext();
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
 }
