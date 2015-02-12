33283df36be5e23419e63ae0609884939c15b920
yoloswagings <arnedebremme@hotmail.com>
2015-02-12 10:45:58 +0100

    views en controller

---
+++ p2groep11.Net/Controllers/SchoolYearController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace p2groep11.Net.Controllers
+{
+    public class SchoolYearController : Controller
+    {
+        // GET: SchoolYear
+        public ActionResult Index()
+        {
+            return View("SchoolYearCreate");
+        }
+    }
+}
