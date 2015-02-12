c54527c6dcebdb8ca358b2296c05bef317ad3c0e
208134ld <l.dupont@hotmail.be>
2015-02-12 21:44:39 +0100

    #8 Toevoegen validatie Viewmodel

--- p2groep11.Net/ViewModels/SchoolYearFormViewModel.cs
+++ p2groep11.Net/ViewModels/SchoolYearFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,19 +9,22 @@ using p2groep11.Net.Models;
 namespace p2groep11.Net.ViewModels
 {
 
-    public class SchoolYearCreateViewModel
+    public class SchoolYearFormViewModel
     {
         public List<SelectListItem> SchoolYears { set; get; }
+
+        [Required]
+        [Display(Name = "Schooljaar")]
         public int SelectedYear { get; set; }
     
 
-        public SchoolYearCreateViewModel(List<SelectListItem> SYears )
+        public SchoolYearFormViewModel(List<SelectListItem> SYears )
         {
             SchoolYears = SYears;
 
         }
 
-        public SchoolYearCreateViewModel()
+        public SchoolYearFormViewModel()
         {
             
         }
