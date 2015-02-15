e5b58ccf9bfb5f2e2871dc39bdf2733bee06033b
yoloswagings <arnedebremme@hotmail.com>
2015-02-15 19:02:19 +0100

    ShowKlimatogram zonder parameters.

--- p2groep11.Net/Controllers/KlimatogramController.cs
+++ p2groep11.Net/Controllers/KlimatogramController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DotNet.Highcharts;
+using DotNet.Highcharts.Enums;
+using DotNet.Highcharts.Helpers;
+using DotNet.Highcharts.Options;
 using p2groep11.Net.Models;
 using p2groep11.Net.ViewModels;
 
@@ -29,8 +34,82 @@ namespace p2groep11.Net.Controllers
             //locaties, landen en locaties uit de repository halen
             return View(new KlimatogramViewModel(GetContinents(), GetCountrys(), GetLocations()));
         }
+        //setUp voor demo als chooseklimatogram nog niet werkt.
+        public ActionResult ShowKlimatogram()
+        {
+            Highcharts chart = new Highcharts("chart")
+               .InitChart(new Chart { ZoomType = ZoomTypes.Xy })
+               .SetTitle(new Title { Text = "klimatogram" })
+               .SetSubtitle(new Subtitle { Text = "text" })
+               .SetXAxis(new XAxis { Categories = new[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" } })
+               .SetYAxis(new[]
+                {
+                    new YAxis
+                    {
+                        Labels = new YAxisLabels
+                        {
+                            Formatter = "function() { return this.value +'°C'; }",
+                            Style = "color: '#89A54E'"
+                        },
+                        Title = new YAxisTitle
+                        {
+                            Text = "Temperature",
+                            Style = "color: '#89A54E'"
+                        },
+                        Opposite = true,
+                        Max = 110
+                    },
+                    new YAxis
+                    {
+                        Labels = new YAxisLabels
+                        {
+                            Formatter = "function() { return this.value +' mm'; }",
+                            Style = "color: '#4572A7'"
+                        },
+                        Title = new YAxisTitle
+                        {
+                            Text = "Rainfall",
+                            Style = "color: '#4572A7'"
+                        },
+                        Max = 220
+                    }
+                })
+               .SetTooltip(new Tooltip
+               {
+                   Formatter = "function() { return ''+ this.x +': '+ this.y + (this.series.name == 'Rainfall' ? ' mm' : '°C'); }"
+               })
+               .SetLegend(new Legend
+               {
+                   Layout = Layouts.Vertical,
+                   Align = HorizontalAligns.Left,
+                   X = 120,
+                   VerticalAlign = VerticalAligns.Top,
+                   Y = 100,
+                   Floating = true,
+                   BackgroundColor = new BackColorOrGradient(ColorTranslator.FromHtml("#FFFFFF"))
+               })
+               .SetSeries(new[]
+                {
+                    new Series
+                    {
+                        Name = "Rainfall",
+                        Color = ColorTranslator.FromHtml("#4572A7"),
+                        Type = ChartTypes.Column,
+                        YAxis = "1",
+                        Data = new Data(new object[] { 49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4 })
+                    },
+                    new Series
+                    {
+                        Name = "Temperature",
+                        Color = ColorTranslator.FromHtml("#DE091E"),
+                        Type = ChartTypes.Spline,
+                        Data = new Data(new object[] { 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6 })
+                    }
+                });
 
-
+            return View(chart);
+        }
+        
 
         //voorlopig om strings in de selectlist te steken
         private List<SelectListItem> GetContinents()
@@ -65,5 +144,6 @@ namespace p2groep11.Net.Controllers
             }
             return locationList;
         }
+        
 	}
 }
