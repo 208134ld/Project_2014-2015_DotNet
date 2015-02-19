9e0cfd9477ead2501d95f3a78264a2a8c3e5f6c6
stijn26 <stijnaerts32@gmail.com>
2015-02-19 15:44:39 +0100

    DummyData en KlimatogramControllerTest gemaakt

---
+++ p2groep11.Net.Tests/Controllers/DummyDataContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Routing;
+using p2groep11.Net.Models.Domain;
+
+namespace p2groep11.Net.Tests.Controllers
+{
+   public class DummyDataContext
+   {
+       public  IList<Continent> Continenten { get; set; }
+       public  Continent Europa { get; private set; }
+
+       public DummyDataContext()
+       {
+           Continent europa = new Continent();
+           europa.ContinentID = 1;
+           europa.Name = "Europa";
+           Europa = europa;
+           Continenten = new List<Continent>
+                          {
+                              europa
+                          };
+       }
+    }
+}
---
+++ p2groep11.Net.Tests/Controllers/KlimatogramControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using p2groep11.Net.Controllers;
+using p2groep11.Net.Models;
+using p2groep11.Net.Models.Domain;
+using p2groep11.Net.ViewModels;
+
+
+namespace p2groep11.Net.Tests.Controllers
+{
+    [TestClass]
+    public class ReservatieControllerTest
+    {
+        private KlimatogramController controller;
+        private Mock<IContinentRepository> continentRepository;
+
+        private Continent continent;
+        private KlimatogramViewModel model;
+        private DummyDataContext context;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            context = new DummyDataContext();
+            continentRepository = new Mock<IContinentRepository>();
+            continentRepository.Setup(c => c.FindById(1)).Returns(context.Europa);
+            continent = context.Europa;
+            controller = new KlimatogramController(continentRepository.Object);
+
+            model = new KlimatogramViewModel(controller.GetContinents(), controller.GetCountrys(),
+                controller.GetLocations());
+
+        }
+
+        [TestMethod]
+        public void ChoosingKlimatogramReturnsModel()
+        {
+            ViewResult result = controller.ChooseKlimatogram() as ViewResult;
+            KlimatogramViewModel kModel = result.Model as KlimatogramViewModel;
+            Assert.AreEqual(context.Continenten, kModel.Continents);
+        }
+    }
+}
