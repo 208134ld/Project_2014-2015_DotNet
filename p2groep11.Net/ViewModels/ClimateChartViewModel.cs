22d481e6ea2c7cfc4e07639aabebf9940389b162
yoloswagings <arnedebremme@hotmail.com>
2015-03-02 13:48:19 +0100

    chart in viewmodel i.p.v domein

--- p2groep11.Net/ViewModels/ClimateChartViewModel.cs
+++ p2groep11.Net/ViewModels/ClimateChartViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using DotNet.Highcharts;
+using DotNet.Highcharts.Enums;
+using DotNet.Highcharts.Helpers;
+using DotNet.Highcharts.Options;
 using p2groep11.Net.Models.Domain;
 
 namespace p2groep11.Net.ViewModels
@@ -17,13 +21,108 @@ namespace p2groep11.Net.ViewModels
         public int SumSed { get; private set; }
         public DeterminateTable table { get; private set; }
         
-        public ClimateChartViewModel(Highcharts chart, IEnumerable<Month> months, DeterminateTable table)
+        public ClimateChartViewModel(ClimateChart c,DeterminateTable table)
         {
-            this.Chart = chart;
-            this.Months = months;
+            this.Months = c.Months;
+            this.Chart = DrawClimateChart(c);
+            
             AvgTemp =  Months.Average(m => m.AverTemp);
-            SumSed = months.Sum(m => m.Sediment);
+            SumSed = Months.Sum(m => m.Sediment);
             this.table = table;
+        }
+        public Highcharts DrawClimateChart(ClimateChart climateChart)
+        {
+            DeterminateTable table = new DeterminateTable();
+
+
+            /*ClimateChart c = climateChart;*/
+
+            int m = climateChart.CalculateMaxForChart();
+            int[] sed = this.Months.Select(p => p.Sediment).ToArray();
+            int[] tem = this.Months.Select(p => p.AverTemp).ToArray();
+            Object[] sediments = new object[12];
+            Object[] temp = new object[12];
+            CopyIntArrayToObjectArray(sed, sediments);
+            CopyIntArrayToObjectArray(tem, temp);
+            Highcharts chart = new Highcharts("chart")
+               .InitChart(new Chart { ZoomType = ZoomTypes.Xy })
+               .SetTitle(new Title { Text = "klimatogram " + climateChart.Location + " (" + climateChart.Country.Name + ")" })
+               .SetSubtitle(new Subtitle { Text = climateChart.BeginPeriod + " - " + climateChart.EndPeriod })
+               .SetXAxis(new XAxis { Categories = new[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" } })
+               .SetYAxis(new[]
+                {
+                    new YAxis
+                    {
+                        Labels = new YAxisLabels
+                        {
+                            Formatter = "function() { return this.value +'°C'; }",
+                            Style = "color: '#DE091E'"
+                        },
+                        Title = new YAxisTitle
+                        {
+                            Text = "Temperatuur",
+                            Style = "color: '#DE091E'"
+                        },
+                        Opposite = true,
+                        Max = climateChart.CalculateMaxForChart()/2,
+                        Min = climateChart.CalculateMinForChart()
+                        
+                    },
+                    new YAxis
+                    {
+                        Labels = new YAxisLabels
+                        {
+                            Formatter = "function() { return this.value +'mm'; }",
+                            Style = "color: '#4572A7'"
+                        },
+                        Title = new YAxisTitle
+                        {
+                            Text = "Neerslag",
+                            Style = "color: '#4572A7'"
+                        },
+                        Max = climateChart.CalculateMaxForChart(),
+                        Min = climateChart.CalculateMinForChart()*2
+                    }
+                })
+               .SetTooltip(new Tooltip
+               {
+                   Formatter = "function() { return ''+ this.x +': '+ this.y + (this.series.name == 'Neerslag' ? ' mm' : '°C'); }"
+               })
+               .SetLegend(new Legend
+               {
+                   Layout = Layouts.Vertical,
+                   Align = HorizontalAligns.Left,
+                   X = 120,
+                   VerticalAlign = VerticalAligns.Top,
+                   Y = 100,
+                   Floating = true,
+                   BackgroundColor = new BackColorOrGradient(ColorTranslator.FromHtml("#FFFFFF"))
+               })
+               .SetSeries(new[]
+                {
+                    new Series
+                    {
+                        Name = "Neerslag",
+                        Color = ColorTranslator.FromHtml("#4572A7"),
+                        Type = ChartTypes.Column,
+                        YAxis = "1",
+                        Data = new Data(sediments)
+                         
+                    },
+                    new Series
+                    {
+                        Name = "Temperatuur",
+                        Color = ColorTranslator.FromHtml("#DE091E"),
+                        Type = ChartTypes.Spline,
+                        Data = new Data(temp)
+                    }
+                });
+
+            return chart;
+        }
+        private void CopyIntArrayToObjectArray(int[] intArray, Object[] objectAr)
+        {
+            intArray.CopyTo(objectAr, 0);
         }
     }
 }
