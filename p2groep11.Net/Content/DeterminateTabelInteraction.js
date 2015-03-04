64783bd0affcb289fffbd74bb2e6d0d39cdef6db
yoloswagings <arnedebremme@hotmail.com>
2015-03-04 14:34:37 +0100

    #test

---
+++ p2groep11.Net/Content/DeterminateTabelInteraction.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function() {
+    $('.clauseTd').on('click', function () {
+        $(this).toggleClass("clauseTdAct");
+    });
+    $('.resultTd').on('click', function () {
+        $(this).toggleClass("resultTdAct");
+    });
+    $('.testBut').on('click', function() {
+            var selectedItems = document.getElementsByClassName('clauseTdAct');
+        alert(selectedItems.length);
+    });
+    
+});
