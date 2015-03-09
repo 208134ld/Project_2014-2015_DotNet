2723e3911a2e97d2e1778baf5c389c9c0db5ebf5
SamuelCaudenberg <scaudenberg@hotmail.com>
2015-03-09 16:04:36 +0100

    DeterminatieTabel 1e graad

---
+++ p2groep11.Net/Models/Domain/TM.cs
@@ -0,0 +1,16 @@
+﻿using p2groep11.Net.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace p2groep11.Net
+{
+    class TM : Parameter
+    {
+        public int Execute(ClimateChart chart)
+        {
+            return chart.MaandenOnder10;
+        }
+    }
+}
