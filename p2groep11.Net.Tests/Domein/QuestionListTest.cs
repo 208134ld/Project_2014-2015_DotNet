c18a1c98dcacbfea3f966c18a10f1c5a0aa1da58
208134ld <l.dupont@hotmail.be>
2015-03-22 23:51:57 +0100

    #32 Testen

---
+++ p2groep11.Net.Tests/Domein/QuestionListTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using p2groep11.Net.Models.Domain;
+
+namespace p2groep11.Net.Tests.Domein
+{
+    [TestClass]
+    public class QuestionListTest
+    {
+        [TestMethod]
+        public void ConstructorMakesNewListParameters()
+        {
+            QuestionList questionList = new QuestionList();
+            List<Parameter> lijst = new List<Parameter>();
+            Assert.AreEqual(lijst, questionList.Parameters);
+
+        }
+    }
+}
